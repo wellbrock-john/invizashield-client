9bfba81273cc0ac7504edd91db512e29637f4e2b
John Wellbrock <wellbrock23john@gmail.com>
2020-12-28 13:20:52 -0700

    added services, service deps, not found route and starter landing route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import LandingRoute from "../../routes/LandingRoute";
+import NotFoundRoute from "../../routes/NotFoundRoute/NotFoundRoute";
 
 export default class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<p>Here is the App Component</p>
+			<div className="App" id="App">
+				<main>
+					{hasError && (
+						<p>
+							There was an error. Don't worry, we're on it! We're sorry for the
+							inconvenience.
+						</p>
+					)}
+					<Switch>
+						<Route component={LandingRoute} />
+						<Route component={NotFoundRoute} />
+					</Switch>
+				</main>
 			</div>
 		);
 	}
