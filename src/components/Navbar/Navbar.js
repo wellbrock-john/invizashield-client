2fac834e93f0d7025c307a124050e5e4018a5779
John Wellbrock <wellbrock23john@gmail.com>
2020-12-30 09:46:56 -0700

    responsive navbar with roughdraft styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,33 +3,40 @@ import { MenuItems } from "../Menu/MenuItems";
 import logo from "../../components/Logo/InvizaShield-Logo.jpg";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { NavButton } from "../NavButton/NavButton";
 
 export default class Navbar extends Component {
-  state = { clicked: false}
+	state = { clicked: false };
 
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked })
-  }
+	handleClick = () => {
+		this.setState({ clicked: !this.state.clicked });
+	};
 
-  render() {
-    return (
-      <nav className="NavbarItems">
-        <Link id="navbar-logo" className="navbar-logo" to="/"><img src={logo} alt="Logo" /></Link>
-        <div className="menu-icon" onClick={this.handleClick}>
-          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-        </div>
-        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={item.cname} href={item.url}>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    );
-  }
+	render() {
+		return (
+			<nav className="NavbarItems">
+				<Link id="navbar-logo" className="navbar-logo" to="/">
+					<img src={logo} alt="Logo" />
+				</Link>
+				<div className="menu-icon" onClick={this.handleClick}>
+					Menu{" "}
+					<i
+						className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+					></i>
+				</div>
+				<ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+					{MenuItems.map((item, index) => {
+						return (
+							<li key={index}>
+								<a className={item.cName} href={item.url}>
+									{item.title}
+								</a>
+							</li>
+						);
+					})}
+				</ul>
+				<NavButton>Sign Up</NavButton>
+			</nav>
+		);
+	}
 }
