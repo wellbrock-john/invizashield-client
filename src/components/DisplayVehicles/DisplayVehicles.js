7fb2f99f4e04c6ff93c97af28350ef4e03df3617
John Wellbrock <wellbrock23john@gmail.com>
2021-01-12 17:08:36 -0700

    updates

--- src/components/DisplayVehicles/DisplayVehicles.js
+++ src/components/DisplayVehicles/DisplayVehicles.js
@@ -5,46 +5,52 @@ import AuthApiService from "../../services/auth-api-service";
 import "./DisplayVehicles.css";
 
 class DisplayVehicles extends Component {
-  static contextType = Context;
+	static contextType = Context;
 
-  handleDelete = (id) => {
-    AuthApiService.deleteVehicle(id);
-    this.context.setVehiclesAfterDelete(id);
-  }
+	handleDelete = (id) => {
+		AuthApiService.deleteVehicle(id);
+		this.context.setVehiclesAfterDelete(id);
+	};
 
-  render() {
-    const { vehicles } = this.context;
+	render() {
+		const { vehicles } = this.context;
 
-    return (
-      <div className="DisplayVehicles">
-        <ul className="vehicles-list">
-          {vehicles.map((vehicle, index) => {
-            return (
-              <li key={index} className="vehicle-data">
-                <p className="data--primary">
-                  {vehicle.year}{" "}
-                  {vehicle.make}{" "}
-                  {vehicle.model}{" "}
-                </p>
-                <Link
-                  className="data-secondary"
-                  to={`/vehicle-management/${vehicle.id}`}
-                >
-                  <strong>--</strong> Edit
-                </Link>
-                <button className="delete-vehicle" type="submit" onClick={() => this.handleDelete(vehicle.id)}>
-                  Delete
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-         <a className="add-vehicle-link" href="/vehicle-management/">
-         <i className="fas fa-shield-alt" /> Add A New Vehicle And Get A Free Quote Here! <i className="fas fa-shield-alt" />
-        </a>
-      </div>
-    );
-  }
+		return (
+			<div className="DisplayVehicles">
+				<ul className="vehicles-list">
+					{vehicles.map((vehicle, index) => {
+						return (
+							<li key={index} className="vehicle-data">
+								<p className="data--primary">
+									{vehicle.year} {vehicle.make} {vehicle.model}{" "}
+								</p>
+								<Link
+									className="data-secondary"
+									to={`/vehicle-management/${vehicle.id}`}
+								>
+									<strong>--</strong> Edit
+								</Link>
+								<button
+									className="delete-vehicle"
+									type="submit"
+									onClick={() => {
+										if (window.confirm("Are you sure?"))
+											this.handleDelete(vehicle.id);
+									}}
+								>
+									Delete
+								</button>
+							</li>
+						);
+					})}
+				</ul>
+				<a className="add-vehicle-link" href="/vehicle-management/">
+					<i className="fas fa-shield-alt" /> Add A New Vehicle And Get A Free
+					Quote Here! <i className="fas fa-shield-alt" />
+				</a>
+			</div>
+		);
+	}
 }
 
 export default withRouter(DisplayVehicles);
