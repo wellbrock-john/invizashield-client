5de15436171041d79745ed851393d3b6b93a4cf0
John Wellbrock <wellbrock23john@gmail.com>
2021-01-19 22:33:15 -0500

    fixed config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line
 export default {
 	PORT: process.env.PORT || 3000,
-	API_ENDPOINT: "http://localhost:8000/api",
-	// API_ENDPOINT: "https://mysterious-badlands-57541.herokuapp.com/api",
+	API_ENDPOINT: "https://mysterious-badlands-57541.herokuapp.com/api",
 	TOKEN_KEY: "invizashield-token",
 };
